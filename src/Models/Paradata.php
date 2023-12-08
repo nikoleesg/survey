37cacec93f491b0ee3a2dbe4f3dc785dbf2365e9
Niko Lee <Niko.lee@outlook.sg>
2023-12-08 17:29:15 +0800

    add ofLabel scope

--- src/Models/Paradata.php
+++ src/Models/Paradata.php
@@ -22,4 +22,110 @@ class Paradata extends Model
     {
         $query->where('interview_number', $interviewNumber);
     }
+
+    public function scopeOfLabel(Builder $query, string $label): void
+    {
+        $query->where('label', '=', $label);
+    }
+
+    public function scopeOfAddress(Builder $query): void
+    {
+        $query->ofLabel('Address');
+    }
+
+    public function scopeOfAddressId(Builder $query): void
+    {
+        $query->ofLabel('AddressId');
+    }
+
+    public function scopeOfClientInformation(Builder $query): void
+    {
+        $query->ofLabel('ClientInformation');
+    }
+
+    public function scopeOfDeviceId(Builder $query): void
+    {
+        $query->ofLabel('DeviceId');
+    }
+
+    public function scopeOfEndReason(Builder $query): void
+    {
+        $query->ofLabel('EndReason');
+    }
+
+    public function scopeOfInterviewEndTime(Builder $query): void
+    {
+        $query->ofLabel('InterviewEndTime');
+    }
+
+    public function scopeOfInterviewStartTime(Builder $query): void
+    {
+        $query->ofLabel('InterviewStartTime');
+    }
+
+
+    public function scopeOfInterviewerId(Builder $query): void
+    {
+        $query->ofLabel('InterviewerId');
+    }
+
+    public function scopeOfLocaleId(Builder $query): void
+    {
+        $query->ofLabel('LocaleId');
+    }
+
+    public function scopeOfQuota(Builder $query): void
+    {
+        $query->ofLabel('Quota');
+    }
+
+    public function scopeOfQuotaVariables(Builder $query): void
+    {
+        $query->ofLabel('QuotaVariables');
+    }
+
+    public function scopeOfSampleData(Builder $query): void
+    {
+        $query->ofLabel('SampleData');
+    }
+
+    public function scopeOfSamplingPointId(Builder $query): void
+    {
+        $query->ofLabel('SamplingPointId');
+    }
+
+    public function scopeOfSurveyETag(Builder $query): void
+    {
+        $query->ofLabel('SurveyETag');
+    }
+
+    public function scopeOfSurveyVersion(Builder $query): void
+    {
+        $query->ofLabel('SurveyVersion');
+    }
+
+    public function scopeOfTestInterview(Builder $query): void
+    {
+        $query->ofLabel('TestInterview');
+    }
+
+    public function scopeOfTimeZone(Builder $query): void
+    {
+        $query->ofLabel('TimeZone');
+    }
+
+    public function scopeOfLocationInfo(Builder $query): void
+    {
+        $query->ofLabel('LocationInfo');
+    }
+
+    public function scopeOfLastLocationInfo(Builder $query): void
+    {
+        $query->ofLabel('LastLocationInfo');
+    }
+
+    public function scopeOfInterviewQuality(Builder $query): void
+    {
+        $query->ofLabel('InterviewQuality');
+    }
 }
