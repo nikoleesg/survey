c226673f1a0b5213c29f17c75befcb136853e077
Niko Lee <Niko.lee@outlook.sg>
2023-12-15 21:40:07 +0800

    add matrix variable type; add labels() method in variableTypeEnum add attributes in migration

--- src/Enums/VariableTypeEnum.php
+++ src/Enums/VariableTypeEnum.php
@@ -10,6 +10,7 @@ enum VariableTypeEnum: int
     case OPEN = 4;
     case ALPHA = 5;
     case CALCULABLE = 6;
+    case MATRIX = 7;
     case DUMMY = 9;
     case DATETIME = 11;
     case DATE = 12;
@@ -25,10 +26,29 @@ enum VariableTypeEnum: int
             VariableTypeEnum::OPEN => 'Open',
             VariableTypeEnum::ALPHA => 'Text',
             VariableTypeEnum::CALCULABLE => 'Calculation',
+            VariableTypeEnum::MATRIX => 'Matrix',
             VariableTypeEnum::DUMMY => 'Dummy',
             VariableTypeEnum::DATETIME => 'Datetime',
             VariableTypeEnum::DATE => 'Date',
             VariableTypeEnum::TIME => 'Time',
         };
+    }
+
+    public static function labels(): array
+    {
+        return [
+            1 => 'Single',
+            2 => 'Multiple',
+            3 => 'Numerical',
+            4 => 'Open',
+            5 => 'Text',
+            6 => 'Calculation',
+            7 => 'Matrix',
+            9 => 'Dummy',
+            11 => 'Datetime',
+            12 => 'Date',
+            13 => 'Time',
+        ];
+
     }
 }
